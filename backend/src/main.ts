503622dc4e726a2deac7c243f2865ed282fddb4b
rallmac <tobijahekperikpe@gmail.com>
2026-03-18 16:21:10 +0100

    swagger endpoint tester initialized

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+     .setTitle('Grand-PE Server')
+     .setDescription('The API of Grand-PE Global')
+     .setVersion('1.0')
+     .addTag('All Services')
+     .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
