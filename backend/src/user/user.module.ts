07ba2be30b05b8b913a7576ee6ffd745791ebcc6
rallmac <tobijahekperikpe@gmail.com>
2026-03-22 16:10:17 +0100

    app modules now exports mongooseModule

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -12,5 +12,6 @@ import { User, UserSchema } from './schema/user.schema';
   ],
   controllers: [UserController],
   providers: [UserService],
+  exports: [MongooseModule],
 })
 export class UserModule {}
