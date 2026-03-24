168e56ca348fdcf578ee25fe7e0b3dff5ebb92ef
rallmac <tobijahekperikpe@gmail.com>
2026-03-24 16:45:06 +0100

    Registration page created. Awaiting modification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import PlantsPage from "./pages/plants";
 import SolarPage from "./pages/solar";
 import TechPage from "./pages/tech";
 import TranslatePage from "./pages/translate";
+import Register from "./pages/Register";
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
         <Route path="/solar" element={<SolarPage />} />
         <Route path="/tech" element={<TechPage />} />
         <Route path="/translate" element={<TranslatePage />} />
+        <Route path="/register" element={<Register />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,12 @@
+import { HeaderGrandpe } from '../components/HeaderGrandpe';
+import { FooterGrandpe } from '../components/FooterGrandpe';
+
+
+export default function Register() {
+	return (
+		<>
+		<HeaderGrandpe />
+		<FooterGrandpe />
+		</>
+	)
+};
