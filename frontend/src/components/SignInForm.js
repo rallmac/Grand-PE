638498c08887d2734b96b4b506764f0f5a6e22d8
rallmac <tobijahekperikpe@gmail.com>
2026-03-26 06:48:09 +0100

    Trailing parentessis fix

--- frontend/src/components/SignInForm.js
+++ frontend/src/components/SignInForm.js
@@ -6,7 +6,7 @@ export default function LoginForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = async (e: React.FormEvent) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
