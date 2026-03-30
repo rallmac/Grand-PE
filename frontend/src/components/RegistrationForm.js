f79dad79e5e92c078b55d3a95fec768c5c7a6496
rallmac <tobijahekperikpe@gmail.com>
2026-03-30 22:23:03 +0100

    improved auth logic for successfull registration

--- frontend/src/components/RegistrationForm.js
+++ frontend/src/components/RegistrationForm.js
@@ -28,7 +28,7 @@ export default function RegistrationForm() {
   };
 
   return (
-    <div className="flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8 bg-gray-900">
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 bg-gray-900">
       
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img
