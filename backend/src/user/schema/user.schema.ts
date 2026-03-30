867b503a4790550509915f620a4307ba92cf26b4
rallmac <tobijahekperikpe@gmail.com>
2026-03-30 19:39:03 +0100

    Invalid or expired token bug fix - re-written user schema

--- backend/src/user/schema/user.schema.ts
+++ backend/src/user/schema/user.schema.ts
@@ -2,19 +2,41 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 @Schema()
 export class User {
-	@Prop()
-	name: string;
-	email: string;
-	password: { type: String, select: false };
-	isVerified: boolean;
-	verificationToken?: string;
-	verificationTokenExpires?: Date;
-	lastVerificationEmailSent?: Date;
-	resetPasswordToken?: string;
-	resetPasswordExpires?: Date;
-	lastResetEmailSent?: Date;
-	refreshToken?: string;
-	refreshTokens?: string[];
+  @Prop()
+  name: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ select: false })
+  password: string;
+
+  @Prop({ default: false })
+  isVerified: boolean;
+
+  @Prop()
+  verificationToken?: string;
+
+  @Prop()
+  verificationTokenExpires?: Date;
+
+  @Prop()
+  lastVerificationEmailSent?: Date;
+
+  @Prop()
+  resetPasswordToken?: string;
+
+  @Prop()
+  resetPasswordExpires?: Date;
+
+  @Prop()
+  lastResetEmailSent?: Date;
+
+  @Prop()
+  refreshToken?: string;
+
+  @Prop({ type: [String] })
+  refreshTokens?: string[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
