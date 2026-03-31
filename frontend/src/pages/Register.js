f3fc2fa5d0eda746ce1a3c3e53b5f1ac9ea390f7
rallmac <tobijahekperikpe@gmail.com>
2026-03-31 22:16:42 +0100

    Improved Registration page

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,14 +1,10 @@
-import { HeaderGrandpe } from '../components/HeaderGrandpe';
-import { FooterGrandpe } from '../components/FooterGrandpe';
 import RegistrationForm from '../components/RegistrationForm';
 
 
 export default function Register() {
 	return (
 		<>
-		<HeaderGrandpe />
 		<RegistrationForm />
-		<FooterGrandpe />
 		</>
 	)
 };
