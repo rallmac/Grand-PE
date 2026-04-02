82a9f4d27bcfdb43dde92bd4b2145f4736dbe495
rallmac <tobijahekperikpe@gmail.com>
2026-04-02 14:44:09 +0100

    Users are automatically logged out after 2 hours of inactivity

--- frontend/src/hooks/IdleTimerHook.js
+++ frontend/src/hooks/IdleTimerHook.js
@@ -1,5 +1,35 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useCallback } from 'react';
 
-export default function (onLogout, timeout = 2 * 60 * 60 * 1000) {
-	
+export default function IdleLogout(onLogout, timeout = 2 * 60 * 60 * 1000) {
+	const timer = useRef(null);
+
+	const resetTimer = useCallback(() => {
+		if (timer.current) clearTimeout(timer.current);
+
+		timer.current = setTimeout(() => {
+			onLogout(); 
+		}, timeout);
+	}, [onLogout, timeout]);
+
+	window.addEventListener('storage', (event) => {
+		if (event.key === 'token' && !event.newValue) {
+			window.location.href = '/signin';
+		}
+	});
+
+	useEffect(() => {
+		const events = ['moveMouse', 'keyDown', 'click', 'scroll'];
+
+		events.forEach((event) =>
+			window.addEventListener(event, resetTimer));
+
+		resetTimer();
+
+		return () => {
+			events.forEach((event) =>
+				window.removeEventListener(event, resetTimer)
+				);
+			if (timer.current) clearTimeout(timer.current);
+		};
+	}, [resetTimer]);
 }
