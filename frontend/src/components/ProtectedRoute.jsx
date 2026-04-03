4d0d46e773ce8f42b498a74c85a9cf860bbd9eed
rallmac <tobijahekperikpe@gmail.com>
2026-04-03 09:35:14 +0100

    If signed in, users are redirected to dashboard if signin page is visited

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
 import { Navigate } from 'react-router-dom';
+import { isTokenValid } from '../hooks/IsTokenValid';
 
 export default function ProtectedRoute({ children }) {
 	const token =
 	  localStorage.getItem('token') ||
 	  sessionStorage.getItem('token');
 
-	if (!token) {
+	if (!isTokenValid(token)) {
 		return <Navigate to='/signin' replace/>;
 	}
 
--- frontend/src/components/PublicRoute.jsx
+++ frontend/src/components/PublicRoute.jsx
@@ -1,12 +1,13 @@
 import { Navigate } from 'react-router-dom';
+import { isTokenValid } from '../hooks/IsTokenValid';
 
 export default function PublicRoute({ children }) {
 	const token =
 	  localStorage.getItem('token') ||
 	  sessionStorage.getItem('token');
 
-	if (token) {
-		<Navigate to='/dashboard' replace />;
+	if (isTokenValid(token)) {
+		return <Navigate to='/dashboard' replace />;
 	}
 
 	return children;
