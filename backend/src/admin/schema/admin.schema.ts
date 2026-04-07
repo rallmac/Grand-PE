3bb2ed02e64001bec90ee4bd81008985ab008d43
rallmac <tobijahekperikpe@gmail.com>
2026-04-07 19:56:14 +0100

    Category and admin schema updated from entity to schema type

--- backend/src/admin/schema/admin.schema.ts
+++ backend/src/admin/schema/admin.schema.ts
@@ -1,4 +1,12 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+
+@Schema()
 export class AdminAllowList{
+
+        @Prop()
 	id: string;
+
+        @Prop()
 	email: string;
 }
--- backend/src/categories/schema/category.schema.ts
+++ backend/src/categories/schema/category.schema.ts
@@ -1,1 +1,12 @@
-export class Category {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+
+@Schema()
+export class Category {
+
+    @Prop()
+    id: string;
+
+    @Prop()
+    name: string;
+}
