798bdb9ed267bc1eef851645cc5867e2254833df
rallmac <tobijahekperikpe@gmail.com>
2026-04-14 20:00:57 +0100

    Modified Order Module - additional imports

--- backend/src/orders/orders.module.ts
+++ backend/src/orders/orders.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schema/order.schema';
+import { Product, ProductSchema } from '../products/schema/product.schema';
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([
+            {name: Order.name, schema: OrderSchema},
+            {name: Product.name, schema: ProductSchema}
+        ]),
+    ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
