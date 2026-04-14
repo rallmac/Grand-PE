9495ddce7779c24f9ed7b5926253eaa717360a5c
rallmac <tobijahekperikpe@gmail.com>
2026-04-14 19:01:10 +0100

    Order Schema defined

--- backend/src/orders/schema/order.schema.ts
+++ backend/src/orders/schema/order.schema.ts
@@ -1,1 +1,32 @@
-export class Order {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Type } from 'mongoose';
+import { User } from '../../user/schema/user.schema';
+
+@Schema({ timestamps: true })
+export class Order {
+    @Prop({ type: Types.ObjectId, ref: 'User', required: true})
+    user: User | Type.ObjectId;
+
+    @Prop([
+        {
+            product: {type: Types.ObjectId, ref: 'Product'},
+            quantity: Number,
+            price: Number,
+        },
+    ])
+    items: {
+        product: Types.ObjectId;
+        quantity: number;
+        price: number;
+    }[];
+
+    @Prop({required: true})
+    totalAmount: number;
+
+    @Prop({default: 'pending'})
+    status: string; //pending | paid | shipped | delivered | cancelled
+}
+
+export type OrderDocument = Order + Document;
+
+export const OrderSchema = SchemaFactory.createForClass(Order);
