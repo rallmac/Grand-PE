29bcdefd3f6983d95eeac964d7fff46e8bf1c7e5
rallmac <tobijahekperikpe@gmail.com>
2026-04-16 22:10:03 +0100

    Chart page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import Verify from "./pages/Verify";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 import PublicRoute from "./components/PublicRoute";
+import Chart from "./pages/Chart";
 
 //This file connects all the pages in the application
 //The connection is done by importing the pages and
@@ -40,6 +41,7 @@ function App() {
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/verify" element={<Verify />} />
         <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/chart" element={<Chart />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
