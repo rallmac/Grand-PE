d54c09486d89f01cea3aea41cea1d7bbfc2d4e21
rallmac <tobijahekperikpe@gmail.com>
2026-04-17 11:10:58 +0100

    Cart. not Chart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ import Verify from "./pages/Verify";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 import PublicRoute from "./components/PublicRoute";
-import Chart from "./pages/Chart";
+import Cart from "./pages/Cart";
 
 //This file connects all the pages in the application
 //The connection is done by importing the pages and
@@ -41,7 +41,7 @@ function App() {
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/verify" element={<Verify />} />
         <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-        <Route path="/chart" element={<Chart />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
