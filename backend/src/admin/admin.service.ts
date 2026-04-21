b37289ea0a83bc92a1763049b2effe280878f543
rallmac <tobijahekperikpe@gmail.com>
2026-04-21 01:29:06 +0100

    admin service is now able to whitelist admin users

--- backend/src/admin/admin.service.ts
+++ backend/src/admin/admin.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+    async whitelistEmail(email: string) {
+        const exists = await this.adminModel.findOne({ email });
+        if (exists) throw new Error('Already whitelisted');
+
+        const admin = this.adminModel.create({ email });
+
+        await this.mailService.approvedAsAdmin(email);
+
+        return admin;
+    }
+
+    async removedFromWhitelist(email: string) {
+        const admin = await this.adminModel.findOneAndDelete({ email });
+
+        if (!admin) throw new Error('Email not found');
+
+        await this.mailService.removedAsAdmin(email);
+
+        return { message: 'Removed successfully' };
+    }
+}
