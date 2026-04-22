c297471ef45465a10ca7d98fef9eb6eb8845f54c
rallmac <tobijahekperikpe@gmail.com>
2026-04-22 08:42:26 +0100

    updated jwt strategy my imorting user schema and other neccessary dependencies

--- backend/src/admin/schema/admin.schema.ts
+++ backend/src/admin/schema/admin.schema.ts
@@ -2,11 +2,13 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 
 @Schema()
-export class AdminAllowList{
+export class AdminAllowlist{
 
         @Prop()
 	id: string;
 
-        @Prop()
+        @Prop({ required: true })
 	email: string;
 }
+
+export const AdminSchema = SchemaFactory.createForClass(AdminAllowlist);
--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -1,12 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from '../user/schema/user.schema';
 
 
 @Injectable()
 export class JwtStrategy extends
 PassportStrategy(Strategy) {
-	constructor() {
+	constructor(
+            @InjectModel(User.name)
+            private userModel: Model<User>,
+        ) {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 			secretOrKey: process.env.JWT_SECRET,
