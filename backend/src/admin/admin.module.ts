db9bd688a1c023c9f7d6a02154942870e5146a52
rallmac <tobijahekperikpe@gmail.com>
2026-04-22 07:52:29 +0100

    Updated admin module with imports of mongoose module and email module

--- backend/src/admin/admin.module.ts
+++ backend/src/admin/admin.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
+import {
+    AdminAllowlist,
+    AdminAllowlistSchema
+} from './schema/admin-allowlist.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { EmailModule } from '../email/email.module';
 
 @Module({
+  imports: [
+      MongooseModule.forFeature([
+          { name: AdminAllowlist.name, schema: AdminAllowlistSchema },
+      ])
+      EmailModule,
+  ],
   controllers: [AdminController],
   providers: [AdminService]
+  exports: [MongooseModule],
 })
 export class AdminModule {}
