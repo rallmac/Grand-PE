8a45a81dbe0ed0e510e3ddc619a72867de3630c4
rallmac <tobijahekperikpe@gmail.com>
2026-04-25 07:45:18 +0100

    superadmin service updated

--- backend/src/superadmin/superadmin.service.ts
+++ backend/src/superadmin/superadmin.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class SuperadminService {}
+export class SuperadminService {
+	constructor(
+	){}
+
+	// ================= APPROVE (SUPERADMIN ONLY) =================
+  async approveAdmin(email: string) {
+    const admin = await this.adminModel.findOne({ email });
+
+    if (!admin) throw new BadRequestException('Admin not found');
+
+    admin.isApproved = true;
+    await admin.save();
+
+    await this.emailService.approveAsAdmin(email);
+
+    return { message: 'Admin approved' };
+  }
+}
+
