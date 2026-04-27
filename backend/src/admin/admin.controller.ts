0a03f7b50695ffd0df589fb744759a6ef4b1211c
rallmac <tobijahekperikpe@gmail.com>
2026-04-27 01:04:51 +0100

    Improved admin controller

--- backend/src/admin/admin.controller.ts
+++ backend/src/admin/admin.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Body, Delete, Post, UseGuards } from '@nestjs/common';
+import { UseGuards, Controller, Post, Body, Delete } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AuthGuard } from '@nestjs/passport';
 import { RolesGuard } from '../auth/guards/roles.guard';
@@ -6,19 +6,36 @@ import { Roles } from '../decorators/roles.decorator';
 
 @Controller('admin')
 export class AdminController {
-    constructor(
-        private adminService: AdminService,
-    ){}
+	constructor(
+		private readonly adminService: AdminService
+	) {}
 
-    @UseGuards(AuthGuard('jwt'), RolesGuard)
-    @Roles('superadmin')
-    @Post('whitelist')
-    add(@Body('email') email: string){
-        return this.adminService.whitelistEmail(email);
-    }
+	@Post('register')
+	async register(@Body('email') email: string){
+		return this.adminService.register(email);
+	}
 
-    @Delete('whitelist')
-    remove(@Body('email') email: string) {
-        return this.adminService.removedFromWhitelist(email);
-    }
+	@Post('login')
+	async login(
+		@Body('email') email: string,
+		@Body('password') password: string
+	){
+		return this.adminService.login(email, password)
+	}
+
+	@Post('set-password')
+	async setPassword(
+		@Body('token') token: string,
+		@Body('password') password: string
+	){
+		return this.adminService.setPassword(token, password)
+	}
+
+	@Post('reset-password')
+	async resetPassword(
+		@Body('token') token: string,
+		@Body('password') password: string
+	){
+		return this.adminService.resetPassword(token, password)
+	}
 }
