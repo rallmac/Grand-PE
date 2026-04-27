a3450d755c49c45c7095e38fa6a532cfee318923
rallmac <tobijahekperikpe@gmail.com>
2026-04-27 01:00:50 +0100

    Improved admin schema

--- backend/src/admin/schema/admin.schema.ts
+++ backend/src/admin/schema/admin.schema.ts
@@ -1,14 +1,33 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
+@Schema({ timestamps: true })
+export class Admin {
+  @Prop({ required: true, unique: true })
+  email: string;
 
-@Schema()
-export class Admin{
-        @Prop()
-        id: string;
-        
-	@Prop({ required: true, unique: true })
-	email: string;
+  @Prop({ select: false })
+  password: string;
+
+  @Prop({ default: false })
+  isVerified: boolean;
+
+  @Prop({ default: false })
+  isApproved: boolean;
+
+  @Prop({ select: false })
+  verificationToken?: string;
+
+  @Prop({ select: false })
+  verificationTokenExpires?: Date;
+
+  @Prop({ select: false })
+  resetPasswordToken?: string;
+
+  @Prop({ select: false })
+  resetPasswordExpires?: Date;
+
+  @Prop({ select: false })
+  refreshToken?: string;
 }
 
-export const AdminSchema =
-SchemaFactory.createForClass(Admin);
+export const AdminSchema = SchemaFactory.createForClass(Admin);
