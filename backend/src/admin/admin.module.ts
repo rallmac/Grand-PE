46a2585250651a8c413870e2248497fc8288fb95
rallmac <tobijahekperikpe@gmail.com>
2026-04-28 22:33:05 +0100

    improved admin module

--- backend/src/admin/admin.module.ts
+++ backend/src/admin/admin.module.ts
@@ -2,21 +2,28 @@ import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import {
-    Admin,
-    AdminSchema
+  Admin,
+  AdminSchema
 } from './schema/admin.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EmailModule } from '../email/email.module';
+import { JwtModule } from '@nestjs/jwt';
+
 
 @Module({
   imports: [
-      MongooseModule.forFeature([
-          { name: Admin.name, schema: AdminSchema },
-      ]),
-      EmailModule,
+    MongooseModule.forFeature([
+      { name: Admin.name, schema: AdminSchema },
+    ]),
+    EmailModule,
+
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '15m' },
+    }),
   ],
   controllers: [AdminController],
   providers: [AdminService],
   exports: [MongooseModule],
 })
-export class AdminModule {}
+export class AdminModule {};
