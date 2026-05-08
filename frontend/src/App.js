1e59b16df6f4a91b5f940dc51a33e4cd3d7db046
rallmac <tobijahekperikpe@gmail.com>
2026-05-08 12:18:08 +0100

    ordinary Users do not need dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,6 @@ import SignIn from "./pages/SignIn";
 import SetPassword from "./pages/SetPassword";
 import ResetPassword from "./pages/ResetPassword";
 import ForgotPassword from "./pages/ForgotPassword";
-import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 import PublicRoute from "./components/PublicRoute";
 import AdminPublicRoute from "./components/AdminPublicRoute";
@@ -49,7 +48,6 @@ function App() {
         <Route path="/set-password" element={<SetPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/new-signup" element={<NewSignup />} />
         <Route path="/ide-page" element={<IdePage />} />
