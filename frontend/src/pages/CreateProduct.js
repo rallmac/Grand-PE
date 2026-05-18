058adf5179fd5c2d8d43b273445e6d2d1a8cf9d1
rallmac <tobijahekperikpe@gmail.com>
2026-05-18 15:23:21 +0100

    Create Product page is now added

---
+++ frontend/src/pages/CreateProduct.js
@@ -0,0 +1,10 @@
+import CreateProductForm from '../component/CreateProductForm';
+
+
+export default function CreateProduct() {
+    return (
+        <>
+        <CreateProductForm />
+        </>
+    )
+};
