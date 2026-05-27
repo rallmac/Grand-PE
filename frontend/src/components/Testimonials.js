09310bd130935acfe57d107a996dab9d03d8fc5c
rallmac <tobijahekperikpe@gmail.com>
2026-05-27 14:30:11 +0100

    More Testimonials

--- frontend/src/components/Testimonials.js
+++ frontend/src/components/Testimonials.js
@@ -17,6 +17,18 @@ export function Testimonials() {
               </p>
               <p>JOSHUA MBAKARA</p>
             </div>
+            <div className="testimonial reveal-on-scroll" style={{ transitionDelay: "0.1s" }}>
+              <p>
+                If you're looking for people you can trust your money to buy good quality solar, look no further!
+              </p>
+              <p>Gloria Solomon</p>
+            </div>
+            <div className="testimonial reveal-on-scroll" style={{ transitionDelay: "0.1s" }}>
+              <p>
+                Highly recommended company for your solar needs with top notch service. Truly professional. You can trust them.
+              </p>
+              <p>Sola Olatinwo</p>
+            </div>
             <div className="text-center reveal-on-scroll" style={{ marginTop: 30 }}>
               <Link to="/about#testimonials" className="btn btn-primary">
                 Read More Testimonials
