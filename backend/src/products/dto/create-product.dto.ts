e3f84248af7c0013aa9b0bcd5cf466d823ebf696
rallmac <tobijahekperikpe@gmail.com>
2026-06-09 15:58:06 +0100

    Create product dto written

--- backend/src/products/dto/create-product.dto.ts
+++ backend/src/products/dto/create-product.dto.ts
@@ -1,1 +1,56 @@
-export class CreateProductDto {}
+import {
+  IsString,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsBoolean,
+  IsMongoId,
+  IsDateString,
+  Min,
+} from 'class-validator';
+
+
+export class CreateProductDto {
+
+  @IsString()
+  @IsNotEmpty()
+  id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsMongoId()
+  category: string;
+
+  @IsString()
+  @IsNotEmpty()
+  image: string;
+
+  @IsNumber()
+  @Min(0)
+  price: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  quantityAvailable?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  quantityOrdered?: number;
+
+  @IsOptional()
+  @IsBoolean()
+  isOutOfStock?: boolean;
+
+  @IsOptional()
+  @IsDateString()
+  createdAt?: Date;
+}
+
