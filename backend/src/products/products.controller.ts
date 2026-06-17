ec44825ad3a2f43e44906df6e2ac6e91ef10a384
rallmac <tobijahekperikpe@gmail.com>
2026-06-17 13:01:08 +0100

    modified create product dto and create product controller

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -1,4 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
+
+import { OrdersService } from '../orders/orders.service';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -6,42 +18,49 @@ import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../decorators/roles.decorator';
 
-
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(
+    private readonly orderService: OrdersService,
+    private readonly productsService: ProductsService
+    ) {}
 
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles('admin')
-  @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
-  }
+  // Admin only
+  //@UseGuards(JwtAuthGuard, RolesGuard)
+  //@Roles('admin')
+  //@Post()
+  //create(@Body() createProductDto: CreateProductDto) {
+  //  return this.productsService.create(createProductDto);
+  //}
 
+  // Public
   @Get()
   findAll() {
     return this.productsService.findAll();
   }
 
+  // Public
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(+id);
   }
 
+  // Admin only
   @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
   @Patch(':id')
-  @Roles('admin')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update('+id', updateProductDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsService.update(id, updateProductDto);
   }
 
+  // Admin only
   @UseGuards(JwtAuthGuard, RolesGuard)
-  @Delete(':id')
   @Roles('admin')
+  @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.productsService.remove(+id);
+    return this.orderService.remove(+id);
   }
-
-  //@Post('order')
-
 }
