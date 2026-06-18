da5792cad9b74d8bb2a3a150a7d8456f3d1e1cd3
rallmac <tobijahekperikpe@gmail.com>
2026-06-18 08:28:52 +0100

    added class transformer

--- backend/src/products/dto/create-product.dto.ts
+++ backend/src/products/dto/create-product.dto.ts
@@ -4,14 +4,13 @@ import {
   IsNumber,
   IsOptional,
   IsBoolean,
-  IsMongoId,
   IsDateString,
   Min,
 } from 'class-validator';
 
+import { Type, Transform } from 'class-transformer';
 
 export class CreateProductDto {
-
   @IsString()
   @IsNotEmpty()
   id: string;
@@ -28,21 +27,28 @@ export class CreateProductDto {
   @IsNotEmpty()
   category: string;
 
+  @Type(() => Number)
   @IsNumber()
   @Min(0)
   price: number;
 
   @IsOptional()
+  @Type(() => Number)
   @IsNumber()
   @Min(0)
   quantityAvailable?: number;
 
   @IsOptional()
+  @Type(() => Number)
   @IsNumber()
   @Min(0)
   quantityOrdered?: number;
 
   @IsOptional()
+  @Transform(({ value }) => {
+    if (typeof value === 'boolean') return value;
+    return value === 'true';
+  })
   @IsBoolean()
   isOutOfStock?: boolean;
 
